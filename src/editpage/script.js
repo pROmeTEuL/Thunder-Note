eb602b033ea1f65f107af106a2e687dcedce2838
Radu Teodor <radu.teodor.vatra@gmail.com>
2024-10-28 23:10:55 +0200

    name conflict resolve + note update functionality

--- src/editpage/script.js
+++ src/editpage/script.js
@@ -1,17 +1,20 @@
+import { api } from '../api/api.js';
+import { log } from '../debugtools/log.js';
+
 const { ipcRenderer } = require('electron');
 const container = document.getElementById('note-container');
 
 const title_td = document.getElementById('title-td');
-const content_td = document.getElementById('content-td');
+const body_td = document.getElementById('body-td');
 const date_td = document.getElementById('date-td');
 
 const title = document.createElement('input');
 title.type = 'text';
 title.classList.add('note-title');
 
-const content = document.createElement('textarea');
-content.style.resize = 'none';
-title.classList.add('note-content');
+const body = document.createElement('textarea');
+body.style.resize = 'none';
+title.classList.add('note-body');
 
 const date = document.createElement('p');
 date.classList.add('note-time');
@@ -22,24 +25,25 @@ title.addEventListener('change', () => {
     changed = true;
 });
 
-content.addEventListener('change', () => {
+body.addEventListener('change', () => {
     changed = true;
 });
-function editNote() {
+
+log('Html connetions ready');
+
+async function editNote() {
+    log('Editing note');
     const noteId = window.location.search.split('=')[1];
-    // const note = await api.get(`/notes/${noteId}`);
-    const note = {
-        title: 'Note title',
-        content: 'Note content',
-        time: '10:30 AM • October 6, 2024'
-    }
+    const note = await api.get(`/${noteId}`);
     title.value = note.title;
-    content.value = note.content;
-    date.textContent = note.time;
+    body.value = note.body;
+    date.textContent = note.date;
     title_td.appendChild(title);
-    content_td.appendChild(content);
+    body_td.appendChild(body);
     date_td.appendChild(date);
 }
+
+log('Note loaded');
 
 function cancelEdit() {
     const noteId = window.location.search.split('=')[1];
@@ -49,12 +53,10 @@ function cancelEdit() {
 function doneEdit() {
     const noteId = window.location.search.split('=')[1];
     if (changed) {
-        date.textContent = new Date().toLocaleString();
-        // api.post(`/notes/${noteId}`, {
-        //     title: title.value,
-        //     content: content.value
-        //     time: date.textContent
-        // });
+        api.put(`/${noteId}`, {
+            title: title.value,
+            body: body.value
+        });
     }
     window.location.assign(`../notepage/page.html?id=${noteId}`);
 }
